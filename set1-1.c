5f98c1acfb0796046d4a3f84adae80c7a6d57962
vigneshnomercy <37579861+vigneshnomercy@users.noreply.github.com>
2018-03-20 16:54:37 +0530

    Create set1-1.c

---
+++ set1-1.c
@@ -0,0 +1,18 @@
+#include <iostream.h>
+#include <conio.h>
+void main(){
+int a;
+printf("enter your number");
+ scanf("%d", &a);
+ if(a==0)
+ {
+ printf("given value is zero");
+ }else if(a=>1){
+  printf("given value is positive");
+  }
+  else
+  { printf("given value is negative");
+  }
+  getch();
+  }
+ 
